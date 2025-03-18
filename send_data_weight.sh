d200fb9fab628c4084d4eca9dd001eb748b32c54
jinlovespho <msjchr922@gmail.com>
2025-03-18 07:49:01 +0000

    20250318 shared repos with team

--- send_data_weight.sh
+++ send_data_weight.sh
@@ -1,2 +1,4 @@
-scp -r ./generated_data cvlab08@163.152.163.126:/home/cvlab08/projects/data/jinlovespho/nips25/github/diffbir_finetuning
+scp -r ./generated_data/TextOCR cvlab08@163.152.163.126:/home/cvlab08/projects/data/jinlovespho/nips25/github/diffbir_finetuning/generated_data
 scp -r ./pretrained_weights cvlab08@163.152.163.126:/home/cvlab08/projects/data/jinlovespho/nips25/github/diffbir_finetuning
+scp -r ./generated_data/TextOCR cvlab12@163.152.163.141:/media/dataset2/jinlovespho/DiffBIR/generated_data
+scp -r ./generated_data/dataset_modified.json cvlab12@163.152.163.141:/media/dataset2/jinlovespho/DiffBIR/generated_data
